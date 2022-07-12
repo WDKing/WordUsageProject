9f18590b615397295a090d28f5977ae2b3158268
Will D King <Will.David.King@gmail.com>
2022-07-11 23:13:02 -0500

    20220711-1112 - Functional word location and returns the count to display on the form.

--- WordUsageForm.cs
+++ WordUsageForm.cs
@@ -1,30 +1,64 @@
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
 namespace WordUsageProject
 {
     public partial class WordUsageProjectForm : Form
     {
+        string[] linesOfNovel = Array.Empty<string>();
+
         public WordUsageProjectForm()
         {
             InitializeComponent();
         }
 
+
         private void WordUsageForm_Load(object sender, EventArgs e)
         {
-            
+
             string[] wordList = File.ReadAllLines(@"TextFiles\stop-word-list.txt");
             foreach (string word in wordList)
             {
                 // TODO Add allowance for comments and empty lines
-                if (!word.Equals("") && word.ToCharArray()[0] != '#' )
+                if (!word.Equals("") && word.ToCharArray()[0] != '#')
                 {
                     WordToLocate.Items.Add(word);
                 }
             }
+
+            linesOfNovel = File.ReadAllLines(@"TextFiles\literature-text.txt");
         }
 
         private void WordToLocate_SelectedIndexChanged(object sender, EventArgs e)
         {
-            // Word to search for: WordToLocate.SelectedItem.ToString();
+            string wordToLocate = WordToLocate.SelectedItem.ToString();
+            
+            Debug.WriteLine($"Word To Locate has changed to: {wordToLocate}");
+            System.Diagnostics.Trace.WriteLine($"Trace - Word To Locate has changed to: {wordToLocate}");
+
+            int wordCount = TallyWordCount(wordToLocate);
+
+            FinalCountTextBox.Text = $"The Word \"{wordToLocate}\" was found {wordCount} times.";
         }
 
+        // Private Methods
+        private int TallyWordCount(string wordToCount)
+        {
+            int tally = 0;
+
+            foreach (string line in linesOfNovel) {
+                Debug.WriteLine($"Line to split: {line}");
+                foreach (var word in Regex.Matches(line, "\\w+('(s|d|t|ve|m|ll))?"))
+                {
+                    Debug.WriteLine($"Word to find: {wordToCount}, Compare to: {word}");
+                    if(wordToCount.ToLower().Equals(word.ToString().ToLower()))
+                    {
+                        tally++;
+                    }
+                }
+            }
+
+            return tally;
+        }
     }
 }
