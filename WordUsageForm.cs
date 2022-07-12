a67efaf593ec6315ec0d7779c252774996fd144a
Will D King <Will.David.King@gmail.com>
2022-07-11 20:12:40 -0500

    20220711-2011 - Functional list of words to choose from from text file

--- WordUsageForm.cs
+++ WordUsageForm.cs
@@ -10,6 +10,13 @@ namespace WordUsageProject
 
         private void WordUsageForm_Load(object sender, EventArgs e)
         {
+            
+            string[] wordList = File.ReadAllLines(@"TextFiles\stop-word-list.txt");
+            foreach (var word in wordList)
+            {
+                // TODO Add allowance for comments and empty lines
+                wordToLocate.Items.Add(word);
+            }
         }
 
         private void wordToLocate_SelectedIndexChanged(object sender, EventArgs e)
